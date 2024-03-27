79de64259beece26ca277b54131f57999a4c3f13
Santiago Rossetti Iñiguez <130598026+santiagorossetti@users.noreply.github.com>
2024-03-27 12:07:01 -0300

    Update Ej1.c

--- Ej1.c
+++ Ej1.c
@@ -19,15 +19,25 @@ int main(int argc, char *argv[]) {
 	int Venta3;
 	double SueldoT;
 	
-	//ejercicio 3
+	// ejercicio 3
 	
 	int nota1;
 	int nota2;
 	int nota3;
-	double CParicales;
-	int CalificacionF;
-	int CalificacionTP;
+	double CParciales;
+	int CalP;
+	int CalTP;
 	double CalFinal;
+	
+	// ejercicio 4
+	int A;
+	int B;
+	 
+	
+	// ejercicio 5
+	int Auto1 = 70;
+	int Auto2 = 150;
+	double PuntoX;
 	
 	printf("Ingrese el ejercicio deseado del 1 al 5\n");
 	scanf("%d" , &ejercicio);
@@ -78,15 +88,51 @@ int main(int argc, char *argv[]) {
 		break;
 		
 		case 3:
-			printf("Ingrese sus 3 parciales de sus calificaciones, su calificación del examen final y la calificación del TP final \n");
-			scanf("%d %d %d %d %d" , &nota1 , &nota2 , &nota3 , &CalificacionF , &CalificacionTP);
+			printf("Ingrese sus 3 calificaciones parciales, su calificaciÃ³n del examen final y la calificaciÃ³n del TP final \n");
+			scanf("%d %d %d %d %d" , &nota1 , &nota2 , &nota3 , &CalP , &CalTP);
 			
-			CParciales =
+			CParciales = (nota1 + nota2 + nota3) / 3;
+			CParciales = CParciales * 0.55;
 			
-			//codigo no terminado
+			CalP = CalP * 0.30;
+			CalTP = CalTP * 0.15;
+			
+			CalFinal = CParciales + CalP + CalTP;
+			
+			printf("La nota final del alumno es %.2f\n" , CalFinal);
+			
 			
 			
 		break;
+		
+		case 4:
+			printf("ingrese dos numeros y le digo los numeros pares entre estos dos\n");
+			scanf("%d %d" , &A , &B);
+			
+			if (A < B){
+				for(int i= A; i <= B; i++) {
+					if(i % 2 == 0) {
+						printf("%d ", i);
+					}	
+				}	
+			} else if (A > B) {
+				for(int i= B; i <= A; i++) {
+					if(i % 2 == 0) {
+						printf("%d ", i);
+					}	
+				}	
+			}
+			
+			
+		break;
+		
+		case 5:
+			
+			PuntoX = (Auto1 + Auto2) / 2;
+			
+			printf("los dos vehiculos se cruzan en el kilometro %.2f\n" , PuntoX);
+		
+		break;	
 	}
 	
 return 0;
